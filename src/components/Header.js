587b15c7daee6e34a02743da202459860d4d9ac4
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-08-17 23:05:59 +0200

    added footer and header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../styles/header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>
+          <a href="/#">memory card</a>
+        </h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
